c5b4b0dbb01c040a24a490f6b40ab171c6f24f34
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2015-12-14 09:01:41 +0300

    Added a hacky support of auto-role

--- lib/redmine_delegate_issue/hooks/controller_issues_new_afterbefore_save.rb
+++ lib/redmine_delegate_issue/hooks/controller_issues_new_afterbefore_save.rb
@@ -14,6 +14,7 @@ class DelegateIssuePluginListener < Redmine::Hook::Listener
 	end
 	def controller_issues_new_after_save(context = {})
 		return unless context[:params]["delegate"] == "true"
+		add_author(context[:issue])
 		@delegated_from = Issue.find_by_id(context[:params]["delegate_from"]);
 		relation = IssueRelation.new(:issue_from => @delegated_from, :issue_to => context[:issue], :relation_type => IssueRelation::TYPE_DELEGATED_TO)
 		unless relation.save
@@ -22,4 +23,21 @@ class DelegateIssuePluginListener < Redmine::Hook::Listener
 	end
 
 	private
+
+	# copied from https://github.com/mephi-ut/redmine_auto_role
+	def add_author(issue)
+		project = Project.find_by_id(issue['project_id'])
+		if project.module_enabled?("auto_roles")
+			Rails.logger.warn("Project ID: #{issue['project_id']}")
+			author_role_id = project.custom_field_value(Setting.plugin_redmine_auto_role['author_autorole_custom_field_id'])
+			unless author_role_id.nil? || author_role_id.empty? || author_role_id == '1'
+				Rails.logger.warn("Adding. Role ID: #{author_role_id}")
+				member         = Member.new
+				member.user    = issue.author
+				member.project = project
+				member.roles  += [Role.find(author_role_id)]
+				member.save
+			end
+		end
+	end
 end
